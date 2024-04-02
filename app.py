6d55bf9cdb17cd8ecb0330f1d4fdbcfc783b2570
3b032104 <3b032104@gm.student.ncut.edu.tw>
2024-04-02 15:25:04 +0800

    Third Commit

--- app.py
+++ app.py
@@ -11,3 +11,17 @@ def index():
 @app.route('/hello')
 def hello():
     return '<h1>Hello flask</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {name}!</h1>'
+
+
+@app.route('/user/<name>/<surname>')
+def user_surname(name, surname):
+    return '<h1>Hello, {name} {surname}!</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
